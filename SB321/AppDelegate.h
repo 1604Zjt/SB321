dce254b026f02b3c39375969aff95cff7196c6ed
1604Zjt <185301525@qq.com>
2016-07-02 14:54:45 +0800

    20160702
    创建项目

---
+++ SB321/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SB321
+//
+//  Created by tarena11 on 16/7/2.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SB321/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SB321
+//
+//  Created by tarena11 on 16/7/2.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
